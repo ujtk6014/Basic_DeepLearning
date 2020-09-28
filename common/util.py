17d4b2c9b36ff7d8c19987da232c68b7317671cf
ujtk6014 <ujtk6014.m2@keio.jp>
2020-09-28 16:12:38 +0900

    finich section_7

--- common/util.py
+++ common/util.py
@@ -38,7 +38,6 @@ def conv_output_size(input_size, filter_size, stride=1, pad=0):
 
 def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     """
-
     Parameters
     ----------
     input_data : (データ数, チャンネル, 高さ, 幅)の4次元配列からなる入力データ
---
+++ section_7/lec.py
@@ -0,0 +1,55 @@
+import sys, os 
+sys.path.append(os.pardir)
+from common.util import im2col
+import numpy as np
+
+x1 = np.random.rand(1,3,7,7)
+coll = im2col(x1,5,5,stride=1, pad=0)
+print(coll.shape)
+
+x2 = np.random.rand(10,3,7,7)
+col2 = im2col(x2,5,5,stride=1,pad=0)
+print(col2.shape)
+
+
+class Convolution:
+    def __init__(self,W,b,stride=1,pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+    
+    def forward(self,x):
+        FN, C, FH, FW = self.W.shape
+        N,C, H, W = x.shape
+        out_h = int(1+(H + 2*self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN,-1).T
+        out = np.dot(col, col_W) + self.b
+
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+        return out
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride = 1, psd = 0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+    def forward(sef, x):
+        N, C, H, W = x.shape
+        out_h = int(1+(H - self.pool_h) / self.stride)
+        out_w = int(1+(W - self.pool_w) / self.stride)
+        col = im2col(x, self.pool_h,self.pool_w,self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        out = np.max(col, axis=1)
+
+        out = out.reshape(N, out_h, out_w, C).transpose(0,3,1,2)
+
+        return out 
+
+        
